6658ed33bd2bf56e514e091236304d3c0fe57832
Tore Sinding Bekkedal <toresbe@gmail.com>
2019-01-07 20:39:39 +0100

    Now with 100% added "you can turn it off" functionality

--- core.cpp
+++ core.cpp
@@ -3,70 +3,71 @@
 #include "libpc2/pc2/pc2.hpp"
 #include "libpc2/pc2/beo4.hpp"
 
-class BeoportPC2Interface: public PC2Interface {
-    void beo4_press(uint8_t keycode) {
-        if (keycode == BEO4_KEY_TV) {
-            this->pc2->device->send_telegram({ 0x24 });
-            this->pc2->device->get_data(); // expect 0x24 0x01
-            this->pc2->device->send_telegram({ 0xe0, 0xc0, 0xc1, 0x01, 0x0b, 0x00, 0x00, \
-                                               0x00, 0x04, 0x03, 0x04, 0x01, 0x01, 0x00, \
-                                               0x9a, 0x00 });
-            this->pc2->expect_ack();
-            this->pc2->device->get_data();
-            this->pc2->device->send_telegram({ 0xe0, 0xc0, 0xc1, 0x01, 0x0b, 0x00, 0x00, 0x00, \
-                                               0x45, 0x07, 0x01, 0x02, 0x0b, 0x00, 0x02, 0x01, \
-                                               0x00, 0x00, 0xea, 0x00 });
-            this->pc2->expect_ack();
-            this->pc2->device->get_data();
-            this->pc2->device->send_telegram({ 0xe0, 0xc0, 0xc1, 0x01, 0x14, 0x00, 0x00, 0x00, \
-                                               0x04, 0x03, 0x04, 0x01, 0x01, 0x01, 0xa4, 0x00});
-            this->pc2->expect_ack();
-            this->pc2->device->get_data();
-            this->pc2->device->send_telegram({ 0xe0, 0xc0, 0xc1, 0x01, 0x14, 0x00, 0x00, 0x00, \
-                                               0x08, 0x00, 0x04, 0xa2, 0x00 });
-            this->pc2->expect_ack();
-            this->pc2->device->get_data();
-            this->pc2->mixer->transmit_from_ml(true);
-            this->pc2->mixer->speaker_power(true);
+class BM5PC2Interface: public PC2Interface {
+        void beo4_press(uint8_t keycode) {
+            if (keycode == BEO4_KEY_TV) {
+                this->pc2->device->send_telegram({ 0x24 });
+                this->pc2->device->get_data(); // expect 0x24 0x01
+                this->pc2->device->send_telegram({ 0xe0, 0xc0, 0xc1, 0x01, 0x0b, 0x00, 0x00, \
+                                                   0x00, 0x04, 0x03, 0x04, 0x01, 0x01, 0x00, \
+                                                   0x9a, 0x00 });
+                this->pc2->expect_ack();
+                this->pc2->device->get_data();
+                this->pc2->device->send_telegram({ 0xe0, 0xc0, 0xc1, 0x01, 0x0b, 0x00, 0x00, 0x00, \
+                                                   0x45, 0x07, 0x01, 0x02, 0x0b, 0x00, 0x02, 0x01, \
+                                                   0x00, 0x00, 0xea, 0x00 });
+                this->pc2->expect_ack();
+                this->pc2->device->get_data();
+                this->pc2->device->send_telegram({ 0xe0, 0xc0, 0xc1, 0x01, 0x14, 0x00, 0x00, 0x00, \
+                                                   0x04, 0x03, 0x04, 0x01, 0x01, 0x01, 0xa4, 0x00});
+                this->pc2->expect_ack();
+                this->pc2->device->get_data();
+                this->pc2->device->send_telegram({ 0xe0, 0xc0, 0xc1, 0x01, 0x14, 0x00, 0x00, 0x00, \
+                                                   0x08, 0x00, 0x04, 0xa2, 0x00 });
+                this->pc2->expect_ack();
+                this->pc2->device->get_data();
+                this->pc2->mixer->transmit_from_ml(true);
+                this->pc2->mixer->speaker_power(true);
+            }
+            // Volume up
+            else if (keycode == 0x60) {
+                this->pc2->mixer->adjust_volume(1);
+            }
+            // Volume down
+            else if (keycode == 0x64) {
+                this->pc2->mixer->adjust_volume(-1);
+            }
+            else if (keycode == 0x0c) {
+                this->pc2->device->send_telegram({0xe0, 0xc0, 0xc1, 0x01, 0x0b, 0x00, 0x00, 0x00, 0x04, 0x03, 0x04, 0x01, 0x01, 0x00, 0x9a, 0x00});
+                this->pc2->device->send_telegram({0xe0, 0xc0, 0xc1, 0x01, 0x0a, 0x00, 0x00, 0x00, 0x11, 0x02, 0x02, 0x01, 0x00, 0xa2, 0x00});
+                this->pc2->device->send_telegram({0xe4, 0x01});
+                this->pc2->mixer->speaker_power(false);
+                this->pc2->device->send_telegram({0xe5, 0x00, 0x00, 0x00, 0x01});
+            }
         }
-        // Volume up
-        else if (keycode == 0x60) {
-            this->pc2->mixer->adjust_volume(1);
-        }
-        // Volume down
-        else if (keycode == 0x64) {
-            this->pc2->mixer->adjust_volume(-1);
-        }
+    };
+
+    static volatile bool keepRunning = 1;
+
+    void intHandler(int dummy) {
+        keepRunning = 0;
     }
-};
 
-static volatile bool keepRunning = 1;
-
-void intHandler(int dummy) {
-    keepRunning = 0;
-}
-
-int main(int argc, char *argv[]) {
-    if(argc == 2) {
-        BeoportPC2Interface iface;
+    int main(int argc, char *argv[]) {
+        BM5PC2Interface iface;
         PC2 pc(&iface);
         pc.open();
 
-        if(argv[1][0] == '1') {
-            pc.mixer->transmit_locally(true);
-            pc.mixer->speaker_power(true);
-        } else if (argv[1][0] == '0') {
-            pc.mixer->speaker_power(false);
-        } else if (argv[1][0] == 'm') {
-//            signal(SIGINT, intHandler);
-//            signal(SIGTERM, intHandler);
-            pc.init();
-            pc.mixer->set_parameters(0x22, 0, 0, 0, false);
-            pc.event_loop(keepRunning);
-            pc.mixer->transmit_locally(false);
-            pc.mixer->speaker_power(false);
-        }
-    } else {
-        printf("Usage: %s (figure out the rest from source)\n", argv[0]);
+        signal(SIGINT, intHandler);
+        signal(SIGTERM, intHandler);
+        pc.init();
+        pc.mixer->set_parameters(0x22, 0, 0, 0, false);
+        pc.mixer->transmit_locally(true);
+        pc.mixer->speaker_power(true);
+        pc.device->send_telegram({0xe0, 0x83, 0xc2, 0x01, 0x14, 0x00, 0x47, 0x00, 0x87, 0x1f, 0x04, 0x47, 0x01, 0x00, 0x00, 0x1f, 0xbe, 0x01, 0x00, 0x00, 0x00, 0xff, 0x00, 0x01, 0x00, 0x03, 0x01, 0x01, 0x01, 0x03, 0x00, 0x02, 0x00, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x7d, 0x00});
+        pc.device->send_telegram({0xe0, 0x80, 0xc1, 0x01, 0x14, 0x00, 0x00, 0x00, 0x40, 0x0b, 0x0b, 0x0a, 0x00, 0x03, 0x14, 0x42, 0x28, 0x00, 0x25, 0x12, 0x18, 0x02, 0x88, 0x00});
+        pc.device->send_telegram({0xe0, 0xc0, 0xc1, 0x01, 0x0b, 0x00, 0x00, 0x00, 0x04, 0x03, 0x04, 0x01, 0x01, 0x00, 0x9a, 0x00});
+        pc.event_loop(keepRunning);
+        pc.mixer->transmit_locally(false);
+        pc.mixer->speaker_power(false);
     }
-}
