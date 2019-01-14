61f08ad23ad55901a98d5bbf127642c820b5d46b
Tore Sinding Bekkedal <toresbe@gmail.com>
2019-01-14 17:37:53 +0100

    add timeout, code is still ugly but at least it does not freeze

--- core.cpp
+++ core.cpp
@@ -7,26 +7,28 @@ class BM5PC2Interface: public PC2Interface {
         void beo4_press(uint8_t keycode) {
             if (keycode == BEO4_KEY_TV) {
                 this->pc2->device->send_telegram({ 0x24 });
-                this->pc2->device->get_data(); // expect 0x24 0x01
+                this->pc2->device->get_data(100); // expect 0x24 0x01
                 this->pc2->device->send_telegram({ 0xe0, 0xc0, 0xc1, 0x01, 0x0b, 0x00, 0x00, \
                                                    0x00, 0x04, 0x03, 0x04, 0x01, 0x01, 0x00, \
                                                    0x9a, 0x00 });
                 this->pc2->expect_ack();
-                this->pc2->device->get_data();
+                this->pc2->device->get_data(100);
                 this->pc2->device->send_telegram({ 0xe0, 0xc0, 0xc1, 0x01, 0x0b, 0x00, 0x00, 0x00, \
                                                    0x45, 0x07, 0x01, 0x02, 0x0b, 0x00, 0x02, 0x01, \
                                                    0x00, 0x00, 0xea, 0x00 });
                 this->pc2->expect_ack();
-                this->pc2->device->get_data();
+                this->pc2->device->get_data(100);
                 this->pc2->device->send_telegram({ 0xe0, 0xc0, 0xc1, 0x01, 0x14, 0x00, 0x00, 0x00, \
                                                    0x04, 0x03, 0x04, 0x01, 0x01, 0x01, 0xa4, 0x00});
                 this->pc2->expect_ack();
-                this->pc2->device->get_data();
+                this->pc2->device->get_data(100);
                 this->pc2->device->send_telegram({ 0xe0, 0xc0, 0xc1, 0x01, 0x14, 0x00, 0x00, 0x00, \
                                                    0x08, 0x00, 0x04, 0xa2, 0x00 });
                 this->pc2->expect_ack();
-                this->pc2->device->get_data();
-                this->pc2->mixer->transmit_from_ml(true);
+                this->pc2->device->get_data(100);
+                this->pc2->device->send_telegram({0xe3, 0xa2, 0xff, 0x00, 0x00});
+                this->pc2->device->send_telegram({0xe5, 0x04, 0x00, 0x00, 0x00});
+                this->pc2->device->send_telegram({0xe4, 0x01});
                 this->pc2->mixer->speaker_power(true);
             }
             // Volume up
